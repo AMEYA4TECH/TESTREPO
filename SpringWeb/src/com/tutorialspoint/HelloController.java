d118856c27572537c1f9055b29a2cb33cae04dd3
Amey <amore@asicentral.com>
2015-12-29 13:34:47 +0530

    First Commit

---
+++ SpringWeb/src/com/tutorialspoint/HelloController.java
@@ -0,0 +1,21 @@
+package com.tutorialspoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class HelloController{
+ 
+   @RequestMapping(method = RequestMethod.GET)
+   public String printHello(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      System.out.println("In hello");
+      return "hello";
+   }
+   
+
+
+}
